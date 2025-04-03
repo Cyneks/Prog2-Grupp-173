0e606b4a61ea46a8ccd0716e99f6098213400086
Cyneks <mattiasl303@gmail.com>
2025-04-03 22:11:53 +0200

    Fixade getPrice() kalkylering

--- LongPlay.java
+++ LongPlay.java
@@ -1,23 +1,21 @@
 import java.time.Year;
 
 public class LongPlay extends Recording{
-
     public LongPlay(String name, String artist, int year, int condition, double price){
-
         super(name, artist, year, condition, price);
-
     }
 
     public String getType(){
-
         return "LP";
-
     }
 
     public double getPrice(){
+        double calculatedPrice = this.getOriginalPrice() * ((double) this.getCondition() / 10) + (Year.now().getValue() - this.getYear()) * 5;
 
-        return this.getOriginalPrice() * this.getCondition() + (Year.now().getValue() - this.getYear()) * 5;
-
+        if (calculatedPrice < 10) {
+            return 10;
+        }
+        
+        return this.getOriginalPrice() * ((double) this.getCondition() / 10) + (Year.now().getValue() - this.getYear()) * 5;
     }
-
 }
