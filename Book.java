430aab34e03be027905d9af35d37c25b0374ab8c
Cyneks <mattiasl303@gmail.com>
2025-04-03 15:04:30 +0200

    Lade till toString() metod

--- Book.java
+++ Book.java
@@ -1,33 +1,36 @@
 public class Book extends Item implements PriceableWithVAT6 {
-    private String author;
-    private double price;
-    private boolean bound;
+    //Ändrade instansvariablerna till final (se klassdiagrammet, små gråa symboler ovanför "f" symbolen)
+    private final String author;
+    private final double price;
+    private final boolean bound;
 
-    public Book(String name, String author, double price, boolean bound)
+    public Book(String name, String author, double price, boolean bound) {
         super(name);
-        this.author =author;
-        this.price =price;
-        this.bound =bound;
-}
+        this.author = author;
+        this.price = price;
+        this.bound = bound;
+    }
 
-    @Override
-    public String getType() {
+    private String getType() {
         return "Book";
     }
-    public String getAuthor() {
+
+    private String getAuthor() {
         return author;
     }
+
     public double getPrice() {
-        double finalPrice = double ?price * 1.3 :price; //Indbunda böcker 30% dyrare
-        return finalPrice * 1.06; //Lägg till 6% moms
+        double finalPrice = isBound() == true ? price * 1.3 : price; //Indbunda böcker 30% dyrare
+        return finalPrice; //Tog bort *1.06 då metoden getPriceWithVAT() sköter det istället - Mattias
     }
-    public boolean isBound() {
+
+    private boolean isBound() {
         return bound;
     }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public void setBound(boolean bound) {
-        this.bound = bound;
+
+    //Tog bort set metoderna då det inte ska finnas några enligt klassdiagrammet - Mattias
+
+    public String toString() {
+        return this.getType()+ ": name=" + this.getName() + ", author=" + this.getAuthor() + ", bound=" + this.isBound() + ", price=" + this.getPrice() + ", price+vat=" + this.getPriceWithVAT();
     }
 }
