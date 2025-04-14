e9773d643a781129c71388a845b68f08ea8e33c2
Cyneks <mattiasl303@gmail.com>
2025-04-14 22:51:43 +0200

    Färdig Searcher.java tillagd

--- Searcher.java
+++ Searcher.java
@@ -1,35 +1,59 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.SortedSet;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Searcher implements SearchOperations {
-  private TreeSet<Recording> set = new TreeSet<>();
+  private HashMap<String, Recording> titleMap = new HashMap<>();
   private HashMap<String, Set<Recording>> artistMap = new HashMap<>();
+  private HashMap<Integer, Set<Recording>> yearMap = new HashMap<>();
+  private HashMap<String, Set<Recording>> genreMap = new HashMap<>();
   private HashSet<String> genreSet = new HashSet<>();
 
   public Searcher(Collection<Recording> data) {
     Collection<Recording> recordings = data;
 
-    set.addAll(recordings);
-
-    //HashMap med artister som keys, set med deras låtar som value
     for (Recording album : recordings) {
+      titleMap.put(album.getTitle(), album);
+
       String artist = album.getArtist();
       if (!artistMap.containsKey(artist)) {
         artistMap.put(artist, new TreeSet<Recording>());
       }
 
+      //HashMap med artister som keys, set med deras album som value
       artistMap.get(artist).add(album);
 
-      //Lägger till alla genrer i en recording till ett HashSet
+
+      int year = album.getYear();
+      if (!yearMap.containsKey(year)) {
+        yearMap.put(year, new TreeSet<Recording>());
+      }
+
+      //HashMap med år som keys, set med album från det året som value
+      yearMap.get(year).add(album);
+
+
       for (String genre : album.getGenre()) {
+        //Lägger till alla genrer i en recording till ett HashSet
         genreSet.add(genre);
+
+        if (!genreMap.containsKey(genre)) {
+          genreMap.put(genre, new TreeSet<Recording>());
+        }
+
+        //HashMap med genrer som keys, set med album med samma genre som value
+        genreMap.get(genre).add(album);
       }
     }
   }
 
   @Override
   public long numberOfArtists() {
-    long numberOfUniqueArtists = set.stream().map((Recording recording) -> recording.getArtist()).distinct().count();
-    return numberOfUniqueArtists;
+    return artistMap.keySet().size();
   }
 
   @Override
@@ -39,8 +63,7 @@ public class Searcher implements SearchOperations {
 
   @Override
   public long numberOfTitles() {
-    long numberOfUniqueTitles = set.stream().map((Recording recording) -> recording.getTitle()).distinct().count();
-    return numberOfUniqueTitles;
+    return titleMap.size();
   }
 
   @Override
@@ -55,38 +78,68 @@ public class Searcher implements SearchOperations {
 
   @Override
   public Recording getRecordingByName(String title) {
-    return set.stream()
-    .filter(recording -> recording.getTitle()
-    .equals(title)).findFirst()
-    .orElse(null);
+    return titleMap.get(title);
   }
 
   @Override
   public Collection<Recording> getRecordingsAfter(int year) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsAfter'");
+    TreeSet<Recording> yearSet = new TreeSet<>();
+    for (int recYear : yearMap.keySet()) {
+      if (recYear >= year) {
+        for (Recording album : yearMap.get(recYear)) {
+          yearSet.add(album);
+        }
+      }
+    }
+    
+    return Collections.unmodifiableCollection(yearSet);
   }
 
   @Override
   public SortedSet<Recording> getRecordingsByArtistOrderedByYearAsc(String artist) {
-    return Collections.unmodifiableSortedSet((SortedSet<Recording>) artistMap.get(artist));
+    if (artistMap.keySet().contains(artist)) {
+      return Collections.unmodifiableSortedSet((SortedSet<Recording>) artistMap.get(artist));
+    }
+
+    return Collections.emptySortedSet();
   }
 
   @Override
   public Collection<Recording> getRecordingsByGenre(String genre) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenre'");
+    if (genreSet.contains(genre)) {
+      return Collections.unmodifiableCollection(genreMap.get(genre));
+    }
+    
+    return Collections.emptySortedSet();
   }
 
   @Override
   public Collection<Recording> getRecordingsByGenreAndYear(String genre, int yearFrom, int yearTo) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getRecordingsByGenreAndYear'");
+    TreeSet<Recording> genreYearSet = new TreeSet<>();
+    for (int recYear : yearMap.keySet()) {
+      if (recYear >= yearFrom && recYear <= yearTo) {
+        for (Recording album : yearMap.get(recYear)) {
+          for (String recGenre : album.getGenre()) {
+            if (recGenre.equals(genre)) {
+              genreYearSet.add(album);
+            }
+          }
+        }
+      }
+    }
+    
+    return Collections.unmodifiableCollection(genreYearSet);
   }
 
   @Override
   public Collection<Recording> offerHasNewRecordings(Collection<Recording> offered) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'offerHasNewRecordings'");
+    TreeSet<Recording> newRecordings = new TreeSet<>();
+    for (Recording recording : offered) {
+      if (!titleMap.keySet().contains(recording.getTitle())) {
+        newRecordings.add(recording);
+      }
+    }
+
+    return Collections.unmodifiableCollection(newRecordings);
   }
 }
