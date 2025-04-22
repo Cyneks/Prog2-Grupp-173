774c98ee046a151d38feec621f9ad37294b2f833
Cyneks <mattiasl303@gmail.com>
2025-04-22 17:02:10 +0200

    importSales implementerad

--- Exercise3.java
+++ Exercise3.java
@@ -1,9 +1,5 @@
-
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.io.*;
 
 public class Exercise3 {
 
@@ -17,8 +13,42 @@ public class Exercise3 {
 
 	}
 
-	public Map<Integer, Double> importSales(String fileName) {
-		return null;
+	public Map<Integer, Double> importSales(String fileName) throws IOException {
+		HashMap<Integer, Double> salesMap = new HashMap<>();
+		
+		var fileIn = new FileInputStream(fileName);
+		var dataIn = new DataInputStream(fileIn);
+
+		int amount = dataIn.readInt();
+
+		for (int i = 0; i < amount; i++) {
+			int first = dataIn.readInt();
+			int second = dataIn.readInt();
+			dataIn.readInt();
+			double fourth = dataIn.readDouble();
+			
+			int[] integers = {first, second};
+
+			String combined = "";
+
+			for (int num : integers) {
+            	if (num < 10) {
+                	combined = combined + "0" + String.valueOf(num);
+            	} else {
+                	combined = combined + String.valueOf(num);
+            	}
+        	}
+
+			if (!salesMap.containsKey(Integer.valueOf(combined))) {
+				salesMap.put(Integer.valueOf(combined), fourth);
+			} else {
+				salesMap.put(Integer.valueOf(combined), salesMap.get(Integer.valueOf(combined)) + fourth);
+			}
+		}
+
+		fileIn.close();
+
+		return salesMap;
 	}
 
 	public List<Recording> getRecordings() {
@@ -30,4 +60,3 @@ public class Exercise3 {
 		this.recordings.addAll(recordings);
 	}
 }
-
