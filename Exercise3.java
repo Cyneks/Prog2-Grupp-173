e997877b8e00c5773bdec68a99cb62b6b7935e2c
Owgren <emilberglund04@icloud.com>
2025-04-24 11:10:02 +0200

    exportRecordings

--- Exercise3.java
+++ Exercise3.java
@@ -1,11 +1,54 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Exercise3 {
 
 	private final List<Recording> recordings = new ArrayList<>();
 
-	public void exportRecordings(String fileName) {
+	public void exportRecordings(String fileName) throws IOException{
+		//Going to use BufferedWriter and pass FileWriter into it
+		//so the program doesnt need to make unnecessary amounts of trips
+		//to the disc over and over again (speed loss) to write (if you only use FileWriter that is)
+		//BufferedWriter buffers all strings and access disc to write in file only if i close or it reaches storage limit per buffer
+		BufferedWriter buffer = new BufferedWriter(new FileWriter(fileName));
+
+		StringBuilder fileText = new StringBuilder();
+
+		
+
+		for (Recording currentRec : recordings){
+
+			fileText.append("<recording> \n  ");
+			fileText.append("<artist>" + currentRec.getArtist() + "</artist>\n  ");
+			fileText.append("<title>" + currentRec.getTitle() + "</title>\n  ");
+			fileText.append("<year>" + currentRec.getYear() + "</year>\n  ");
+			fileText.append("<genres>\n    ");
+
+			int idx = 0;
+			int idxOfLastGenre = currentRec.getGenre().size() - 1;
+			for (String currentGenre : currentRec.getGenre()){
+
+				if (idx == idxOfLastGenre) fileText.append("<genre>" + currentGenre + "</genre>\n  ");
+				else fileText.append("<genre>" + currentGenre + "</genre>\n    ");
+				idx++;
+
+			}
+
+			fileText.append("</genres>\n");
+			fileText.append("</recording>\n");
+
+		}
+
+		buffer.write(fileText.toString());
+		buffer.close();
 
 	}
 
