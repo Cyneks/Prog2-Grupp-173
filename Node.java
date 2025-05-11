d144a8d41b37bfc91a94f9c2ec0455807ec31bd0
Cyneks <79505844+Cyneks@users.noreply.github.com>
2025-05-11 17:18:11 +0200

    La till koordinat variabler

--- Node.java
+++ Node.java
@@ -1,15 +1,25 @@
+package se.su.inlupp;
+
 import java.util.Objects;
 
 public class Node {
     private String name;
+    private double coordX;
+    private double coordY;
 
-    public Node(String name) {
+    public Node(String name, double coordX, double coordY) {
         this.name = name;
+        this.coordX = coordX;
+        this.coordY = coordY;
     }
 
     public String getName() {
         return name;
     }
+
+    public double getCoordX() { return coordX; }
+
+    public double getCoordY() { return coordY; }
 
     public boolean equals(Object obj) {
         if (obj instanceof Node node) {
